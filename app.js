971029f9ff7f82a93dcb53415ed5923f05c871c9
adrian <adrianraycasas@gmail.com>
2017-11-30 12:05:02 +0800

    turned on ssl

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ var myport = 3000;
 
 // c o n f i g u r a t i o n 
 
-// var key = fs.readFileSync(path.join(__dirname, '/keys/private.key'));
-// var cert = fs.readFileSync(path.join(__dirname, '/keys/primary.crt'));
-// var ca = fs.readFileSync(path.join(__dirname, '/keys/intermediate.crt'));
+var key = fs.readFileSync(path.join(__dirname, '/keys/private.key'));
+var cert = fs.readFileSync(path.join(__dirname, '/keys/primary.crt'));
+var ca = fs.readFileSync(path.join(__dirname, '/keys/intermediate.crt'));
 
-// var options = {
-//     key: key,
-//     cert: cert,
-//     ca: ca
-// };
+var options = {
+    key: key,
+    cert: cert,
+    ca: ca
+};
 
-// https.createServer(options, app).listen(443, function () {
-//     console.log('https on port 443');
-// });
+https.createServer(options, app).listen(443, function () {
+    console.log('https on port 443');
+});
 
 app.set('views', './application/view');
 app.set('view engine', 'pug')
@@ -45,11 +45,11 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-// var site = "http://localhost:" + myport;
-// proxy.register("immcoin.io", site);
-// proxy.register("www.immcoin.io", site);
-// proxy.register("http://.immcoin.io", site);
-// proxy.register("https://.immcoin.io", site);
+var site = "http://localhost:" + myport;
+proxy.register("immcoin.io", site);
+proxy.register("www.immcoin.io", site);
+proxy.register("http://.immcoin.io", site);
+proxy.register("https://.immcoin.io", site);
 
 // r o u t i n g
 
